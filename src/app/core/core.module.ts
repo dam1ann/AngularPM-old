1027f708d23550ee7fc1475f8d83fe4245b7d59d
Damian <damiannnurzynski@gmail.com>
2020-01-13 23:06:43 +0100

    Add http module to core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
     declarations: [],
     imports: [
-        CommonModule
+        CommonModule,
+        HttpClientModule
     ]
 })
 export class CoreModule {
