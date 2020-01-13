5ce5afbfc3b0a3a90d0511473f6e5dfcfef50fef
Damian <damiannnurzynski@gmail.com>
2020-01-13 22:25:40 +0100

    Update lazy loading routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
-
 const routes: Routes = [
     {
+        path: '',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    },
+    {
         path: 'profile',
-        loadChildren: './profile/profile.module#ProfileModule'
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
     }, {
         path: 'article',
-        loadChildren: './article/article.module#ArticleModule'
+        loadChildren: () => import('./article/article.module').then(m => m.ArticleModule)
     }
 ];
 
